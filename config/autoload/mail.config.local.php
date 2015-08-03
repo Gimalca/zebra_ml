61ee308a1a2ac8cc772abed73ac9c146ae4c14de
oscar <oaps212@gmail.com>
2015-08-03 17:00:54 -0430

    mail

---
+++ config/autoload/mail.config.local.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	'mail' => array(
+		'transport' => array(
+			'options' => array(
+				'host'              => 'p3plcpnl022.prod.phx3.secureserver.net',
+				'connection_class'  => 'login',
+				'connection_config' => array(
+					'username' => 'info@gimalca.com',
+					'password' => 'Gimalc@123',
+                                        'port' => 465,
+					'ssl' => 'tls'
+				),
+			),  
+		),
+	),
+);
